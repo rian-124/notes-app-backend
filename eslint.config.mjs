246d9d6c66e6975444b49840df35e08acfe1c094
rian-124 <afriyanlionel0@gmail.com>
2025-08-14 10:29:03 +0700

    restructure with hapi plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 import daStyle from 'eslint-config-dicodingacademy';
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   daStyle,
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ['src/**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
 ];
